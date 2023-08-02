223ec51054559b4de074513ddb878d196f70697a
H-Shaikh <hamzashaikh9795@gmail.com>
2023-08-02 21:21:03 +0530

    new database mysql and models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guest = models.IntegerField(default=6)
+    booking_date = models.DateTimeField()
+    
+    class Meta:
+        verbose_name = 'Booking'
+        verbose_name_plural = 'Bookings'
+
+    def __str__(self) -> str:
+        return f'{self.name} for {self.number_of_guests} guests on {self.booking_date}'
+    
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    inventory = models.IntegerField(default=5)
+    
+    class Meta:
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menu Items'
+        
+        def __str__(self) -> str:
+            return f'{self.title} : {str(self.price)}'
+        
