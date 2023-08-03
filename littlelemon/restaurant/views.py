0bc5664e12e0da62071fa4db6e1e7a66cfc7814a
H-Shaikh <hamzashaikh9795@gmail.com>
2023-08-03 21:01:04 +0530

    serializers and Menu api

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serilizers import BookingSerializer, MenuSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class BookingView(APIView):
+    def get(self,request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data) # return json
+    
+    
+    
+class MenuView(APIView):
+    def post(self, request):
+        serilizer = MenuSerializer(data=request.data)
+        
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response({"status": "success", "data": serilizer.data})
+    
+
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
